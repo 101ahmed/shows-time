6842d2c31f3230bcc7e37c34ea9b22b40e03a909
Ahmed Abdelrahman <alnonoahmed16@gmail.com>
2025-01-03 11:37:38 +0100

    First comment

--- src/main/java/com/abdel/moviesshostime/MoviesShosTimeApplication.java
+++ src/main/java/com/abdel/moviesshostime/MoviesShosTimeApplication.java
@@ -2,12 +2,31 @@ package com.abdel.moviesshostime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
+//@CrossOrigin(origins = "http://localhost:3000")
 public class MoviesShosTimeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MoviesShosTimeApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(false).maxAge(3600);
+            }
+        };
+    }
 }
